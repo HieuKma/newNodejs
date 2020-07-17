2162b19adaebc6aead90d682df760660657c0e38
hieuitkma <hieuitkma@gmail.com>
2020-07-17 20:33:40 +0700

    Add controllers

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,50 +1,17 @@
 var express = require('express');
 
-const shortid = require('shortid');
+var controller = require('../controllers/user.controller');
 
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-
-const adapter = new FileSync('db.json');
-const db = low(adapter);
-
-db.defaults({ users: [] })
-  .write()
 var router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('users/index2', {
-        users: db.get('users').value()
-    });
-});
+router.get('/', controller.index);
 
-router.get('/search', (req, res) => {
-    let q = req.query.q;
-    let matchedUsers = db.get('users').value().filter(user => {
-        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-    });
-    res.render('users/index2', {
-        users: matchedUsers
-    });
-});
+router.get('/search', controller.search);
 
-router.get('/create', (req, res) => {
-    res.render('users/create');
-});
+router.get('/create', controller.create);
 
-router.get('/:id', (req, res) => {
-    var id = req.params.id;
-    var user = db.get('users').find({ id: id }).value();
+router.get('/:id', controller.getId);
 
-    res.render('users/view', {
-        user: user
-    })
-});
-
-router.post('/create', (req, res) => {
-    req.body.id = shortid.generate();
-    db.get('users').push(req.body).write();
-    res.redirect('/users');
-});
+router.post('/create', controller.postUser);
 
 module.exports = router;
